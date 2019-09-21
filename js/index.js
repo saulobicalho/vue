fad8fb4cf84f938549d2e1b71da744c8c4516b0a
unknown <sbicalho86@gmail.com>
2019-09-21 19:35:52 -0300

    introduces vue

--- js/index.js
+++ js/index.js
@@ -50,3 +50,32 @@
      $('.js-add').click(onAdd);
      $('.js-livro').click(toggleRemovido);
  });
+
+ // Comando 1
+ var data = {
+  livros: [{ titulo: 'Orange is The New Black', autor: 'Piper Kerman', checked: true },
+          { titulo: 'A Origem das Espécies', autor: 'Charles Darwin', checked: false }],
+  cabecalho: 'Livros Preferidos',
+  novoLivro: '',
+  novoAutor: ''
+};
+ // Comando 2
+ new Vue({
+    el: '#app',
+    data: data,
+    methods: {
+      addLivro: function () {
+        var titulo = this.novoLivro.trim();
+        var autor = this.novoAutor.trim();
+        if (titulo && autor) {
+          this.livros.push({
+            titulo: titulo,
+            autor: autor,
+            checked: false
+          });
+          this.novoLivro = '';
+          this.novoAutor = '';
+        }
+      }
+    }
+  });
